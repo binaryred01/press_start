f9dde82aa27bebfb0929ad96d9accd113dd11de7
Danny <mtme13@hotmail.com>
2019-08-07 14:58:09 +0100

    UI: Add back button & option text to option page

---
+++ dragons-of-the-caribbean/DragonOptionPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace dragonsofthecaribbean
+{
+    public partial class DragonOptionPage : ContentPage
+    {
+        public DragonOptionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
